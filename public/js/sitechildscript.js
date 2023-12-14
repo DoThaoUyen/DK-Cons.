eab2acfd17ea3c895666adc7d8d371b44f2b11d9
DoThaoUyen <34001212+DoThaoUyen@users.noreply.github.com>
2023-12-14 14:31:56 +0700

    Add files via upload

--- public/js/sitechildscript.js
+++ public/js/sitechildscript.js
@@ -9,3 +9,14 @@ window.onresize =  function() {
     parent.postMessage(l_height + 10 ,'*');
     console.log(l_height, l_height+10);
 }
+const _clk_zalo = document.getElementById('hs-content-2-1-zalo');
+const _clk_fb = document.getElementById('hs-content-2-1-fb');
+
+_clk_fb.addEventListener('click', function(){
+    window.open('https://www.facebook.com/profile.php?id=61554538439652', '_blank');
+   
+});
+_clk_zalo.addEventListener('click', function(){
+    window.open('https://zalo.me/3850258166399151098', '_blank');
+   
+});
